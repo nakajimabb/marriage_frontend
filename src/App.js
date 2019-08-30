73293a2b263ff1059bc09dc6b45871d732234863
Shinichi Nakajima <nakajimabb@gmail.com>
2019-08-30 17:31:44 +0900

    Material App - Admin & Dashboard Template 1.0.4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
 
-function App() {
+import DateFnsUtils from "@date-io/date-fns";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { StylesProvider } from "@material-ui/styles";
+import { ThemeProvider } from "styled-components";
+
+import maTheme from "./theme";
+import Routes from "./routes/Routes";
+
+function App({ theme }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StylesProvider injectFirst>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <MuiThemeProvider theme={maTheme[theme.currentTheme]}>
+          <ThemeProvider theme={maTheme[theme.currentTheme]}>
+            <Routes />
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </MuiPickersUtilsProvider>
+    </StylesProvider>
   );
 }
 
-export default App;
+export default connect(store => ({ theme: store.themeReducer }))(App);
