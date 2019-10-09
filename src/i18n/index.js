a9bb29e2275427b053ca21f617cbaf7092b8787d
Shinichi Nakajima <nakajimabb@gmail.com>
2019-10-09 20:03:22 +0900

    add some i18n data

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -20,5 +20,12 @@ i18next.init({
 i18next.attr = (...args) => i18next.t('activerecord.attributes.' + args.join('.'));
 i18next.enum = (...args) => i18next.t('enum.' + args.join('.'));
 i18next.model = (...args) => i18next.t('activerecord.models.' + args.join('.'));
+i18next.data_list = (...args) => {
+  let data = i18next.getDataByLanguage(i18next.language).translation;
+  for(let key of args) {
+    data = data[key];
+  }
+  return data;
+};
 
 export default i18next;
--- src/i18n/locales/ja.js
+++ src/i18n/locales/ja.js
@@ -1,5 +1,9 @@
 import merge from 'deepmerge'
+import country from './ja/country.yml';
 import dict from './ja/dict.yml';
+import lang from './ja/lang.yml';
+import message from './ja/message.yml';
+import prefecture from './ja/prefecture.yml';
 import user from './ja/user.yml';
 
-export const ja = merge.all([dict, user]).ja;
+export const ja = merge.all([country, dict, lang, message, prefecture, user]).ja;
