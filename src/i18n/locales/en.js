5cf4ee8e4a7042eb9018f012fc8ea4a003ae2b14
Shinichi Nakajima <nakajimabb@gmail.com>
2019-10-11 21:29:11 +0900

    modify english locale file

--- src/i18n/locales/en.js
+++ src/i18n/locales/en.js
@@ -1,4 +1,9 @@
 import merge from 'deepmerge'
+import country from './en/country.yml';
+import dict from './en/dict.yml';
+import lang from './en/lang.yml';
+import message from './en/message.yml';
+import prefecture from './en/prefecture.yml';
 import user from './en/user.yml';
 
-export const en = merge.all([user]).en;
+export const en = merge.all([country, dict, lang, message, prefecture, user]).en;
